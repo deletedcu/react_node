a4e4d49b4ad3639f3bcda08deb1cd67fcf4a20fd
Curest <43941751+curest0x1021@users.noreply.github.com>
2018-10-01 15:37:21 +0400

    [FE] set up redux (thunk)

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,15 +4,19 @@ import {
   BrowserRouter,
   Route,
 } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import store from './redux/store';
 
 import './styles/styles.css'
 
 class App extends Component {
   render () {
     return (
+      <Provider store={ store }>
         <BrowserRouter>
           <Route path='/' component={Root} />
         </BrowserRouter>
+      </Provider> 
     )
   }
 }
---
+++ front-end/src/redux/store.js
@@ -0,0 +1,7 @@
+import { applyMiddleware, createStore } from 'redux'
+import thunk from 'redux-thunk'
+import reducer from './reducers'
+
+const middleware = applyMiddleware(thunk)
+
+export default createStore(reducer, middleware)
