13c942f44a09181058cc2c9738ca6f2e8de98331
Curest <43941751+curest0x1021@users.noreply.github.com>
2018-10-03 19:08:10 +0400

    [FE] horizontal selection grid component for meal plans page

---
+++ front-end/src/components/HorizontalSelectionGrid/index.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+import classNames from 'classnames'
+
+import './styles.css'
+
+class HorizontalSelectionGrid extends Component {
+
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      selectedIndex: 0,
+    }
+  }
+
+  onSelect = (index) => {
+    this.setState({
+      selectedIndex: index,
+    })
+  }
+
+  render () {
+    const values = this.props.values
+
+    return (
+      <div className='selection-grid'>
+       {
+         values.map((value, index) => {
+          return (<div 
+                    key={ index }
+                    className={ classNames('selection-grid-cell', 'clickable', {'selection-grid-cell-selected': this.state.selectedIndex === index}, {'selection-grid-cell-has-separator': index < values.length - 1}) }
+                    onClick={ () => {this.onSelect(index)} }
+                    >
+                    { value }
+                  </div>
+          )
+         })
+       }
+      </div>
+    )
+  }
+}
+
+export default HorizontalSelectionGrid
