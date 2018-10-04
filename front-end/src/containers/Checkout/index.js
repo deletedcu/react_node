a736ccf974314d0bb0c7de753f5c29bab5392e4b
Curest <43941751+curest0x1021@users.noreply.github.com>
2018-10-04 16:54:56 +0400

    [FE] checkout page main frame

--- front-end/src/containers/Checkout/index.js
+++ front-end/src/containers/Checkout/index.js
@@ -4,10 +4,42 @@ import './styles.css'
 
 class Checkout extends Component {
 
+  onHelpCenter = () => {
+
+  }
+
+  onContactUs = () => {
+    this.props.history.push('/contact-us')
+  }
+
   render () {
     return (
       <div className='div-checkout-container'>
+        {/* Title */}
+        <div className='div-checkout-title'>
+          Checkout
+        </div>
 
+        {/* Checkout Forms */}
+        <div className='div-checkout-forms'>
+          {/* Steps 1-2-3 */}
+          <div className='div-checkout-steps-section'>
+          </div>
+
+          {/* Summary Section */}
+          <div className='div-checkout-summary-section'>
+          </div>
+        </div>
+
+        {/* Separator */}
+        <div className='div-checkout-separator'/>
+
+        {/* Help/Contact Instruction */}
+        <div className='div-help-container'>
+          Need help? Visit the
+          <span className='clickable' onClick={this.onHelpCenter}> Help Center </span> or
+          <span className='clickable' onClick={this.onContactUs}> Contact Us </span>
+        </div>
       </div>
     )
   }
