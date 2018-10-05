6de89ebf5090e4d1606b91359533c370e778bfc2
Curest <43941751+curest0x1021@users.noreply.github.com>
2018-10-05 12:09:50 +0400

    [FE] checkout step component

---
+++ front-end/src/containers/Checkout/components/CheckoutStepForm/index.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react'
+import classNames from 'classnames'
+import Button from '../../../../components/Button'
+
+import './styles.css'
+
+import imgCheck from '../../../../assets/images/right.svg'
+
+export const CheckoutFormStatus = {
+  'editing': 1,
+  'disabled': 2,
+  'completed': 3,
+}
+
+class CheckoutStepForm extends Component {
+
+  onSubmit = (e) => {
+    e.preventDefault()
+
+    this.props.onSubmit()
+  }
+
+  render () {
+    const formStatus = this.props.status
+
+    return (
+      <div className={ classNames('checkout-step-form-container', this.props.className) }>
+        {/* Header */}
+        <div className={ classNames('checkout-step-form-header', {'checkout-step-form-header-inactive': formStatus !== CheckoutFormStatus.editing }) }>
+          { formStatus === CheckoutFormStatus.completed ?
+            <img className='img-checked' src={ imgCheck } alt='check'/>
+            :
+            <div className={ classNames('div-step-number', {'div-step-number-inactive': formStatus === CheckoutFormStatus.disabled}) }>
+              { this.props.number }
+            </div>
+          }
+          <div className='div-checkout-step-title'>
+            { this.props.title }
+          </div>
+        </div>
+
+        { formStatus === CheckoutFormStatus.editing &&
+          <div>
+            {/* Separator */}
+            <div className='checkout-step-form-separator'/>
+
+            {/* Body */}
+            <div className='checkout-step-form-body'>
+              <form onSubmit={ this.onSubmit }>
+                {/* Form */}
+                <div className='div-checkout-step-form'>
+                  { this.props.children }
+                </div>
+
+                {/* Separator */}
+                <div className='checkout-step-form-separator'/>
+
+                {/* Button Area */}
+                <div className='div-checkout-step-form-footer'>
+                  <Button className='btn-checkout-step-form'>{ this.props.buttonTitle }</Button>
+                </div>
+              </form>
+            </div>
+          </div>
+        }
+      </div>
+    )
+  }
+}
+
+export default CheckoutStepForm
