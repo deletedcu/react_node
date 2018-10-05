796bfc268dfd1db5f779ce87bbcd0bc1d61af1d4
Curest <43941751+curest0x1021@users.noreply.github.com>
2018-10-05 16:42:39 +0400

    [FE] checkout - create account section.

--- front-end/src/containers/Checkout/index.js
+++ front-end/src/containers/Checkout/index.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react'
 import OrderSummary from './components/OrderSummary'
+import CheckoutStepForm, { CheckoutFormStatus } from './components/CheckoutStepForm'
 
 import './styles.css'
 
+const CheckoutStep = {
+  'account': 1,
+  'address': 2,
+  'payment': 3,
+}
 class Checkout extends Component {
+
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      currentStep: CheckoutStep.account,
+    }
+  }
 
   onHelpCenter = () => {
 
@@ -13,7 +27,15 @@ class Checkout extends Component {
     this.props.history.push('/contact-us')
   }
 
+  onCreateAccount = () => {
+    this.setState({
+      currentStep: CheckoutStep.address,
+    })
+  }
+
   render () {
+    const currentStep = this.state.currentStep
+
     return (
       <div className='div-checkout-container'>
         {/* Title */}
@@ -25,6 +47,59 @@ class Checkout extends Component {
         <div className='div-checkout-forms'>
           {/* Steps 1-2-3 */}
           <div className='div-checkout-steps-section'>
+
+            {/* Create Account Section */}
+            <CheckoutStepForm
+              number='1'
+              status={ currentStep === CheckoutStep.account ? CheckoutFormStatus.editing : CheckoutFormStatus.completed }
+              title='Create Account'
+              buttonTitle='Proceed to Delivery Info'
+              onSubmit={ this.onCreateAccount }
+            >
+              <div className='div-checkout-account'>
+                <div className='div-checkout-account-names'>
+                  <div className='div-checkout-account-firstname'>
+                    <div><span>First Name</span></div>
+                    <div><input required type='text'/></div>
+                  </div>
+                  <div className='div-checkout-account-lastname'>
+                    <div><span>Last Name</span></div>
+                    <div><input required type='text'/></div>
+                  </div>
+                </div>
+
+                <div className='div-checkout-account-email'>
+                  <div><span>Email Address</span></div>
+                  <div><input required type='email'/></div>
+                </div>
+
+                <div className='div-checkout-account-password'>
+                  <div><span>Password</span></div>
+                  <div><input required type='password'/></div>
+                </div>
+
+                <div className='div-checkout-account-login'>
+                  <span>Already have an account? </span>
+                  <span className='clickable'>Login</span>
+                </div>
+              </div>
+            </CheckoutStepForm>
+
+            {/* Deliver Address Section */}
+            <CheckoutStepForm
+              number='2'
+              status={ currentStep === CheckoutStep.address ? CheckoutFormStatus.editing : (currentStep === CheckoutStep.account ? CheckoutFormStatus.disabled : CheckoutFormStatus.completed) }
+              title='Delivery Address'
+              buttonTitle='Proceed to Payment Info'
+            />
+
+            {/* Payment Info Section */}
+            <CheckoutStepForm
+              number='3'
+              status={ currentStep === CheckoutStep.payment ? CheckoutFormStatus.editing : CheckoutFormStatus.disabled }
+              title='Payment Info'
+              buttonTitle='Complete Order'
+            />
           </div>
 
           {/* Summary Section */}
