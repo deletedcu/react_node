798d5da0201606b79d567be8e1e9e9f71039a9b5
Curest <43941751+curest0x1021@users.noreply.github.com>
2018-10-15 15:44:35 +0400

    [FE] FOUC

--- front-end/src/components/AsyncComponent.js
+++ front-end/src/components/AsyncComponent.js
@@ -1,38 +1,54 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
-/**
- * For component lazy loading, define async component class
- */
-
-export default function asyncComponent (importComponent) {
-
-  class AsyncComponent extends Component {
-
-    constructor (props) {
-      super(props)
-
-      this.state = {
-        component: null,
-      }
-    }
-
-    async componentDidMount () {
-      const { default: component } = await importComponent()
-
-      this.setState({
-        component: component
-      })
-    }
-
-    render () {
-      const C = this.state.component
-
-      return C
-        ? <C {...this.props} />
-        : null
-    }
-
+class AsyncImport extends Component {
+  static propTypes = {
+    load: PropTypes.func.isRequired,
   }
 
-  return AsyncComponent
+  state = {
+    component: null
+  }
+
+  toggleFoucClass () {
+    const root = document.getElementById('root')
+    console.log(root);
+    
+    if (root.className.includes('fouc')) {
+      root.className = root.className.replace('fouc', '')
+    } else {
+      root.className = root.className + ' fouc'
+    }
+  }
+
+  componentWillMount () {
+    this.toggleFoucClass()
+  }
+
+  componentDidMount () {
+    this.props.load()
+      .then((component) => {
+        setTimeout(() => this.toggleFoucClass(), 0)
+        this.setState(() => ({
+          component: component.default
+        }))
+      })
+  }
+
+  render () {
+    return this.props.children(this.state.component)
+  }
 }
+
+const asyncRoute = (importFunc) =>
+  (props) => (
+    <AsyncImport load={importFunc}>
+      {(Component) => {
+        return Component === null
+          ? null
+          : <Component {...props} />
+      }}
+    </AsyncImport>
+  )
+
+export default asyncRoute
