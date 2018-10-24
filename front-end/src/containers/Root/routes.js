bbf0176de32d1564cfc596de4356e01881d3b4f2
Curest <43941751+curest0x1021@users.noreply.github.com>
2018-10-24 17:07:42 +0400

    [FE] remove original /meal-plans  routing

--- front-end/src/containers/Root/routes.js
+++ front-end/src/containers/Root/routes.js
@@ -5,7 +5,7 @@ import asyncComponent from '../../components/AsyncComponent'
 
 const AsyncHome = asyncComponent(() => import('../Home'))
 const AsyncMenu = asyncComponent(() => import('../Menus'))
-const AsyncMealPlans = asyncComponent(() => import('../MealPlans'))
+// const AsyncMealPlans = asyncComponent(() => import('../MealPlans'))
 const AsyncContactUs = asyncComponent(() => import('../ContactUs'))
 const AsyncCheckout = asyncComponent(() => import('../Checkout'))
 
@@ -17,7 +17,7 @@ const routes = (
   <Switch>
     <Route path='/home' component={ AsyncHome } />
     <Route path='/menus' component={ AsyncMenu } />
-    <Route path='/meal-plans' component={ AsyncMealPlans } />
+    {/* <Route path='/meal-plans' component={ AsyncMealPlans } /> */}
     <Route path='/contact-us' component={ AsyncContactUs } />
     <Route path='/checkout' component={ AsyncCheckout } />
 
