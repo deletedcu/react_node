3b3b5be2e72dd8b57bf0a87c30b1a37621b80953
Curest <43941751+curest0x1021@users.noreply.github.com>
2018-11-02 12:02:30 +0400

    [FE] clean up

--- front-end/src/containers/Menus/components/MenuItem/index.js
+++ front-end/src/containers/Menus/components/MenuItem/index.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import NumericCounter from '../../../../components/NumericCounter'
-import Button from '../../../../components/Button'
 
 import './styles.css'
 
