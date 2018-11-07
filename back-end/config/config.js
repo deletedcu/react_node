17de73fb1cd5f8a3181468c933bc60c1f98abde2
Curest <43941751+curest0x1021@users.noreply.github.com>
2018-11-07 11:35:09 +0400

    [BE] moltin basic configuration

--- back-end/config/config.js
+++ back-end/config/config.js
@@ -1,5 +1,7 @@
 const config = {
   jwtSecret: 'mealpost',
+  moltin_client_id: '',
+  moltin_client_secret: '',
 };
 
 module.exports = config;
---
+++ back-end/helpers/moltin.js
@@ -0,0 +1,7 @@
+const config = require('../config/config');
+const MoltinGateway = require('@moltin/sdk').gateway;
+
+const Moltin = MoltinGateway({
+  client_id: config.moltin_client_id,
+  client_secret: config.moltin_client_secret,
+});
