d6e6ff97094c425ba41da8d10148a79eaa80d1dc
Curest <43941751+curest0x1021@users.noreply.github.com>
2018-11-08 16:25:44 +0400

    [BE] make /product/all  public.  return image / files in response.  filtered response

--- back-end/routes/api/product/product.js
+++ back-end/routes/api/product/product.js
@@ -4,17 +4,63 @@ const config = require('../../../config/config');
 
 exports.getAllProducts = (request) => {
   return new Promise((resolve, reject) => {
-    if (checkToken(request)) {
-      Moltin.Products.With(['main_image', 'files']).All()
-        .then(products => {
-          resolve({ status: 200, products: products });
-        })
-        .catch(err => {
-          console.log(err);
-          reject({ status: 500, message: 'Internal Server Error!'});
+    Moltin.Products.With(['main_image', 'files']).All()
+      .then(res => {
+        let products = res.data;
+        let mainImages = res.included.main_images;
+        let files = res.included.files;
+        let result = [];
+
+        products.forEach(product => {
+          if (product.relationships.main_image) {
+            // product has main image, find its url and attach to result
+
+            let mainImageId = product.relationships.main_image.data.id;
+            let mainImage = mainImages.find(mainImage => mainImage.id === mainImageId);
+            product.main_image = mainImage.link.href;
+          } else {
+            // product doesn't have main image
+
+            product.main_image = '';
+          }
+          
+          if (product.relationships.files) {
+            // product has files, find their urls and attach to result
+
+            let fileReferenceList = product.relationships.files.data;
+            let fileUrls = [];
+            
+            fileReferenceList.forEach(fileReference => {
+              let file = files.find(file => file.id === fileReference.id);
+              fileUrls.push(file.link.href);
+            });
+
+            product.files = fileUrls;
+          } else {
+            // product doesn't have attachment files
+
+            product.files = [];
+          }
+
+          result.push({
+            id: product.id,
+            name: product.name,
+            slug: product.slug,
+            sku: product.sku,
+            description: product.description,
+            price: product.price,
+            status: product.status,
+            meta: product.meta,
+            main_image: product.main_image,
+            files: product.files,
+          });
         });
-    } else {
-      reject({ status: 401, message: 'Unauthorized request!'});
-    }
+
+        resolve({ status: 200, products: result });
+      })
+      .catch(err => {
+        console.log(err);
+        reject({ status: 500, message: 'Internal Server Error!'});
+      });
   });
 }
