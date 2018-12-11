91f4372dd21a42e22972829404b6ed6b3944875c
Curest <43941751+curest0x1021@users.noreply.github.com>
2018-12-11 10:07:14 +0400

    [FE] fix styling issues for menu images

--- front-end/src/components/LazyImage/index.js
+++ front-end/src/components/LazyImage/index.js
@@ -7,7 +7,7 @@ const LazyImage = (props) => {
     <div className={ props.className } style={ props.style } onClick={ props.onClick }>
       <Image 
         style={{ paddingTop: 'unset', width: '100%', height: '100%', backgroundColor: 'transparent'}} 
-        imageStyle={{objectFit: 'contain'}}
+        imageStyle={{objectFit: 'cover'}}
         src={ props.src } 
         loading={<ChasingDots size={48} color='#56B05A' />}
         disableSpinner={ props.disableSpinner }
