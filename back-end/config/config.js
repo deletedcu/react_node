d804416b2d610bce491ba1ab729316a19791423c
Curest <43941751+curest0x1021@users.noreply.github.com>
2019-01-07 17:57:12 +0400

    [BE] reverse admin / support email

--- back-end/config/config.js
+++ back-end/config/config.js
@@ -2,9 +2,9 @@ const config = {
   jwtSecret: 'mealpost',
   moltin_client_id: 'xHcn39vic8BO5WIWw9bFCCRBGHGsZ68Ml4fBTDvfbB',
   moltin_client_secret: 'PDzWnVV3EQiT2f5RgjxwEZbYPAs2piKnuPtNjV7Qjl',
-  website_email: 'support@mealpost.io',
-  website_email_password: 'mealpost441',
-  admin_email: 'admin@mealpost.io',
+  website_email: 'admin@mealpost.io',
+  website_email_password: 'SimbaPost123_',
+  admin_email: 'support@mealpost.io',
 };
 
 module.exports = config;
