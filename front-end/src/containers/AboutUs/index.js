3c7b2bc4812e4c769f22faeb6c5e5a4416207870
Curest <43941751+curest0x1021@users.noreply.github.com>
2019-01-10 11:43:43 +0400

    [FE] update about us page

--- front-end/src/containers/AboutUs/index.js
+++ front-end/src/containers/AboutUs/index.js
@@ -19,11 +19,11 @@ class AboutUs extends Component {
           <LazyImage className='img-background' src={ imgBanner } disableSpinner={true} />
           <div className='about-us-description'>
             <div className='about-us-description-title'>We work hard to create delicious food that meet your highest standards.</div>
-            <div className='about-us-description-content'>All our meals are certified organic, non-GMO, and are free of artificial flavors, colors, additives, added nitrates, and gluten. Our menus vary according to the season, but be assured that no matter the season, all meats are antibiotic-free, and when possible, fish is wild-caught.</div>
+            <div className='about-us-description-content'>All our meals are certified organic, non-GMO, and are free of artificial flavors, colors, additives, and added nitrates. Our menus vary according to the season, but be assured that no matter the season, all meats are antibiotic-free, and when possible, fish is wild-caught. We're very passionate about providing you with the highest quality food from local sources.</div>
           </div>
           <div className='about-us-description'>
             <div className='about-us-description-title'>Farm to table means fresh, so our meals are never frozen and never contain preservatives.</div>
-            <div className='about-us-description-content'>Our farm to table meal delivery program is one of the finest available. We don’t just deliver meals, we’re dedicated to providing you with meals that are chef-prepared from the highest quality sustainably sourced, local ingredients. Northern California produces some of the choicest food in the nation. We diligently source ingredients from a team of dedicated farmers who take extraordinary pride in growing their food organically, sustainably, and humanely. Our talented professional chefs are inspired to transform these quality products into delicious, nutritious, carefully prepared meals for your discerning palate. </div>
+            <div className='about-us-description-content'>We don’t just deliver meals, we work some miracles too.  Northern California produces some of the choicest food in the nation. We diligently source ingredients from a team of dedicated farmers who take extraordinary pride in growing their food organically, sustainably, and humanely. Our talented team of professional chefs are inspired to transform these quality products into delicious, nutritious, and carefully prepared meals for your discerning palate. </div>
           </div>
           <div className='about-us-description'>
             <div className='about-us-description-title'>Packaging that really matter..</div>
@@ -31,13 +31,13 @@ class AboutUs extends Component {
           </div>
           <div className='about-us-description'>
             <div className='about-us-description-title'>We believe in Transparency.</div>
-            <div className='about-us-description-content'>Each meal is labeled with an ingredients list, heating instructions, and nutrition facts, curated by our in-house nutritionist.</div>
+            <div className='about-us-description-content'>Each meal is nutritionist labeled with an ingredients list, heating instructions, and accurate nutrition facts.</div>
           </div>
-          <div className='about-us-description-single'>Pure food, pure nutrition, and purely delicious, from farm to table.</div>
+          <div className='about-us-description-single'>Pure food, pure nutrition, and purely delicious.</div>
           <LazyImage className='img-background-2' src={imgBanner2} disableSpinner={true} />
           <div className='about-us-description'>
             <div className='about-us-description-title'>The Vision</div>
-            <div className='about-us-description-content'>We’re guided by our customers’ and community’s welfare. We know that nurturing our bodies with nutritious food and nurturing our souls by committing to socially responsible endeavors creates happy consumers and a sustainable environment for the future.</div>
+            <div className='about-us-description-content'>We’re guided by the welfare of our customers and of our community. We believe that we can make the world a better place by delivering nutritious food in an environmentally and socially responsible way.</div>
           </div>
           <div className='about-us-description'>
             <div className='about-us-description-title'>Our Thought Process..</div>
@@ -45,7 +45,7 @@ class AboutUs extends Component {
           </div>
           <div className='about-us-description'>
             <div className='about-us-description-title'>Combining our shared values and rich backgrounds in technology</div>
-            <div className='about-us-description-content'>With our commitment to staying healthy and eating well, it eventually led us to building our own on-demand healthy delivery service, Mealpost. We figured out how to deliver nutritional high end cuisine on demand to hungry customers around the Bay Area. How? We established trusted partnerships with the best purveyors and local chefs. Our engineering team designed our digitized delivery network in order to work in sync with our platform to serve our hungry customer base.</div>
+            <div className='about-us-description-content'>Ecological, socially responsible, and sustainable business practices are at the heart of everything we do, and our customers come first in every decision we make. Our aim is to ensure that you, our valued customers, trust us to provide you with the finest quality food and delivery service possible. That’s who we are – a team of dedicated perfectionists who developed a better way to eat. We combined farm to table quality and seriously delicious food with customer-centered on-demand convenience – so you can relax and do more of what you love.</div>
           </div>
 
           <div className='separator'/>
